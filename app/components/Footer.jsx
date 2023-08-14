075526eb8647efdfaf57410118dd068e263af55b
Todd H. Albert <todd@bocacode.com>
2023-08-14 11:28:08 -0400

    fix footer, add more tech icons

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,17 +6,14 @@ export default function Footer() {
           <span className='ml-3 text-xl'>ToddAlbert.com</span>
         </a>
         <p className='text-sm text-zinc-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-zinc-200 sm:py-2 sm:mt-0 mt-4'>
-          © 2023 Todd Albert —
-          <a
-            href='https://twitter.com/toddalbert'
-            className='text-zinc-600 transition hover:text-zinc-400 ml-1'
-            rel='noopener noreferrer'
-            target='_blank'>
-            @toddalbert
-          </a>
+          © 2023 Todd Albert
         </p>
         <span className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
-          <a className='text-zinc-500 transition hover:text-zinc-400'>
+          <a
+              href='https://www.facebook.com/ToddHAlbert'
+              target='_Blank'
+              rel='nofollow noreferrer'
+              className='text-zinc-500 transition hover:text-zinc-400'>
             <svg
               fill='currentColor'
               strokeLinecap='round'
@@ -27,7 +24,11 @@ export default function Footer() {
               <path d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z'></path>
             </svg>
           </a>
-          <a className='ml-3 text-zinc-500 transition hover:text-zinc-400'>
+          <a
+              href='https://twitter.com/toddalbert'
+              target='_Blank'
+              rel='nofollow noreferrer'
+              className='ml-3 text-zinc-500 transition hover:text-zinc-400'>
             <svg
               fill='currentColor'
               strokeLinecap='round'
@@ -38,7 +39,11 @@ export default function Footer() {
               <path d='M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z'></path>
             </svg>
           </a>
-          <a className='ml-3 text-zinc-500 transition hover:text-zinc-400'>
+          <a
+              href='https://www.instagram.com/todd.albert/'
+              target='_Blank'
+              rel='nofollow noreferrer'
+              className='ml-3 text-zinc-500 transition hover:text-zinc-400'>
             <svg
               fill='none'
               stroke='currentColor'
--- app/components/Tech.jsx
+++ app/components/Tech.jsx
@@ -3,7 +3,7 @@ export default function Tech() {
     <section data-aos="fade-up" data-aos-duration="500" data-aos-offset="100">
       <div className='py-8 lg:py-16 mx-auto my-auto max-w-screen-xl px-4'>
         <h3 className='mb-4 lg:mb-16 text-3xl font-medium tracking-tight leading-tight text-center text-zinc-900 dark:text-zinc-200 md:text-4xl'>
-          Some of the tech I've been using lately
+          Some of the tech I've been enjoying most lately
         </h3>
         <div className='grid grid-cols-2 gap-8 text-zinc-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-zinc-400'>
           <span className='flex justify-center items-center'>
@@ -22,6 +22,13 @@ export default function Tech() {
           </span>
           <span className='flex justify-center items-center'>
             <img
+              className='h-14 brightness-50 contrast-50 grayscale hover:scale-150 transition'
+              alt='React'
+              src='/tech/react.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
               className='h-9 grayscale brightness-200 contrast-0 hover:scale-150 transition'
               alt='Tailwind CSS'
               src='/tech/Tailwind_CSS_Logo.svg'
@@ -34,11 +41,6 @@ export default function Tech() {
               src='/tech/expressjs.svg'
             />
           </span>
-          {/*
-            Unity
-            VR
-            AR
-          */}
           <span className='flex justify-center items-center'>
             <img
               className='h-12 grayscale brightness-200 contrast-0 hover:scale-150 transition'
@@ -51,6 +53,62 @@ export default function Tech() {
               className='h-12 grayscale brightness-100 contrast-50 hover:scale-150 transition'
               alt='Unity'
               src='/tech/unity.png'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 brightness-50 contrast-100 hover:scale-150 transition'
+              alt='TypeScript'
+              src='/tech/typescript.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-200 contrast-0 hover:scale-150 transition'
+              alt='Solidity'
+              src='/tech/solidity.png'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-200 contrast-0 hover:scale-150 transition'
+              alt='Python'
+              src='/tech/Python.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-100 contrast-50 hover:scale-150 transition'
+              alt='Scikit learn'
+              src='/tech/Scikit_learn.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-200 contrast-0 hover:scale-150 transition'
+              alt='Mongo DB'
+              src='/tech/mongodb.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-100 contrast-50 hover:scale-150 transition'
+              alt='Firestore'
+              src='/tech/Firestore.svg'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-100 contrast-50 hover:scale-150 transition'
+              alt='Terraform'
+              src='/tech/terraform.png'
+            />
+          </span>
+          <span className='flex justify-center items-center'>
+            <img
+              className='h-12 grayscale brightness-100 contrast-50 hover:scale-150 transition'
+              alt='Expo'
+              src='/tech/expo.png'
             />
           </span>
         </div>

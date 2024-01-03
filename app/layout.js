8e6225b54014feddf88f79287de674aac6586a09
Todd H. Albert <todd@bocacode.com>
2024-01-03 10:31:11 -0500

    hopefully fix og-image

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,25 @@ import { albert } from './fonts';
 import 'aos/dist/aos.css';
 
 export const metadata = {
-  title: 'Todd Albert | Senior Engineer / Architect',
-  description: 'Former NASA Research Fellow and Professor',
+  title: 'Todd Albert | Hands-on Software Engineering Leader',
+  description: 'Todd is a Former NASA Research Fellow and Professor with over 30 years of coding experience and 14 years of leadership.',
+  openGraph: {
+    title: 'Todd Albert | Hands-on Software Engineering Leader',
+    description: 'Todd is a Former NASA Research Fellow and Professor with over 30 years of coding experience and 14 years of leadership.',
+    url: 'https://www.toddalbert.com/',
+    siteName: 'toddalbert.com',
+    images: [
+      {
+        url: 'https://www.toddalbert.com/ta-og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
+
 
 export default function RootLayout({ children }) {
   return (
